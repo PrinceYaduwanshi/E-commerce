dac23ee755f9eebd00ac2ddb392d6bd63a9967b2
PrinceYaduwanshi <rajprince9250@gmail.com>
2024-12-28 21:28:01 +0530

    Index Page Designed

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const ejsMate = require("ejs-mate");
 
 app.set("view engine" , "ejs");
 app.set("views" , path.join(__dirname , "/views"));
+app.use(express.static(path.join(__dirname,"/public")));
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
--- init/data.js
+++ init/data.js
@@ -1,73 +1,73 @@
 const sampleProducts = [
     {
         title:"Lemon",
-        image:"https://unsplash.com/photos/sliced-lemon-on-white-background-enNffryKuQI",
+        image:"https://images.unsplash.com/flagged/photo-1587302164675-820fe61bbd55?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         description:"Sour",
         price:50
     },
     {
         title:"Brown Biscuits",
-        image:"https://unsplash.com/photos/four-brown-biscuits-on-white-background-y7iHZ4nY63s",
+        image:"https://plus.unsplash.com/premium_photo-1667621221004-e344ae82ad7e?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         description:"Sweet",
         price:150
     },
     {
         title:"Avocado",
-        image:"https://unsplash.com/photos/black-round-fruits-in-clear-glass-bowl-C6JhUKs9q8M",
+        image:"https://media.istockphoto.com/id/2168638529/photo/fresh-avocado-fruit-on-a-wooden-board.jpg?s=2048x2048&w=is&k=20&c=btYk3FA__PLT5jQlonTVbQdxYfiOuz-227WA35Im-CA=",
         description:"Healthy",
         price:500
     },
     {
         title:"Watch",
-        image:"https://unsplash.com/photos/a-black-and-gold-rolex-watch-on-a-white-background-O703kpzIsQI",
+        image:"https://images.unsplash.com/photo-1639006570490-79c0c53f1080?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         description:"Premium Looks",
         price:2000
     },
     {
         title:"Perfume",
-        image:"https://unsplash.com/photos/n5-chanel-eau-de-parfum-spray-bottle-potCPE_Cw8A",
+        image:"https://images.unsplash.com/photo-1541643600914-78b084683601?q=80&w=1904&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         description:"Smells Good",
         price:250
     },
     {
         title:"T-shirts",
-        image:"https://unsplash.com/photos/a-white-shirt-hanging-on-a-clothes-line-XYtuOYfIg_M",
+        image:"https://images.unsplash.com/photo-1714070700737-24acfe6b957c?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         description:"Men Tshirts",
         price:300
     },
     {
         title:"Jeans",
-        image:"https://unsplash.com/photos/blue-and-white-textile-on-white-textile-EDSTj4kCUcw",
+        image:"https://images.unsplash.com/photo-1604176354204-9268737828e4?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         description:"Men Jeans",
         price:1550
     },
     {
         title:"Denim Fit",
-        image:"https://unsplash.com/photos/a-pair-of-jeans-and-a-jacket-laying-on-the-sand-WOZR7D-osHU",
+        image:"https://plus.unsplash.com/premium_photo-1727285730879-c1cd2781a765?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         description:"Fit Check",
         price:2050
     },
     {
         title:"Denim Jacket",
-        image:"https://unsplash.com/photos/blue-denim-button-up-jacket-QRN47la37gw",
+        image:"https://images.unsplash.com/photo-1611312449408-fcece27cdbb7?q=80&w=1938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         description:"Unisexual",
         price:500
     },
     {
         title:"Chairs",
-        image:"https://unsplash.com/photos/white-plastic-armchair-on-white-floor-5i5aVCrMnkc",
+        image:"https://images.unsplash.com/photo-1590605464732-c291c01c9db9?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         description:"Unisexual",
         price:250
     },
     {
         title:"Laptop",
-        image:"https://unsplash.com/photos/laptop-computer-beside-coffee-mug-j4uuKnN43_M",
+        image:"https://images.unsplash.com/photo-1484788984921-03950022c9ef?q=80&w=1864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         description:"High Graphcs",
         price:650000
     },
     {
         title:"Mouse",
-        image:"https://unsplash.com/photos/magic-mouse-on-white-surface-KuvEVL7lXYQ",
+        image:"https://media.istockphoto.com/id/501518098/photo/computer-wireless-mouse.jpg?s=2048x2048&w=is&k=20&c=X6HsZTYExWh52AAKL1GSuHwBLgzEI2uLj71zMJ4vZAs=",
         description:"Battery Operated",
         price:250
     },
